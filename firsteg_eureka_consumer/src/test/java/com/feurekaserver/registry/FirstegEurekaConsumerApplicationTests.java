d1c5c24feb6ae2e33ef297298743cbb52bb7a444
RishabhUdemy <59793716+RishabhUdemy@users.noreply.github.com>
2023-05-10 23:33:04 +0530

    first commit

---
+++ firsteg_eureka_consumer/src/test/java/com/feurekaserver/registry/FirstegEurekaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feurekaserver.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstegEurekaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
