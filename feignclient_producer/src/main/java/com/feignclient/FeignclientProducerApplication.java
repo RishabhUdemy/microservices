fc0b3a97ddf38aa6bd02d5e4223afd60a5d13fa3
RishabhUdemy <59793716+RishabhUdemy@users.noreply.github.com>
2023-05-16 22:40:25 +0530

    feign client producer example

---
+++ feignclient_producer/src/main/java/com/feignclient/FeignclientProducerApplication.java
@@ -0,0 +1,15 @@
+package com.feignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FeignclientProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignclientProducerApplication.class, args);
+	}
+
+}
---
+++ feignclient_producer/src/main/java/com/feignclient/controller/EmpFeignClientController.java
@@ -0,0 +1,17 @@
+package com.feignclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpFeignClientController {
+
+	@Value("${server.port}")
+	private String portValue;
+	
+	@GetMapping("/printportDetail")
+	public String printPortDetail() {
+		return "The value is coming from:"+portValue;
+	}
+}
